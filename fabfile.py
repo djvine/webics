1d4d21cb5f4388561d05727cbc1ddc6e0edde48e
David Vine <djvine@gmail.com>
2014-01-14 19:22:24 -0600

    Added fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import lcd, local
+
+home = '/home/david'
+
+def prepare_deployment(branch_name):
+    local('python manage.py test webics')
+    local('git add -p && git commit')
+
+def deploy():
+    with lcd('{:s}/web/prod/webics'.format(home)):
+
+        # With git...
+        local('git pull {:s}/web/dev/webics/'.format(home))
+
+        # With both
+        local('python manage.py migrate scans')
+        local('python manage.py test scans')
+        local('sudo service apache2 restart')
