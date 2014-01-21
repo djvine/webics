b83c364f54fa71a0448eaad5b9bf14cc79943e1e
David Vine <djvine@gmail.com>
2014-01-21 12:08:04 -0600

    Plot is looking better  but not complete.

--- webics/static/webics/d3/plot.js
+++ webics/static/webics/d3/plot.js
@@ -1,3 +1,7 @@
+var n_xticks = 10;
+var yScale = 'linear'; // can be pow, log, linear
+var scales = [['linear','Linear'], ['pow','Power'], ['log','Log']];
+
 function hasClass( elem, klass ) {
      return (" " + elem.className + " " ).indexOf( " "+klass+" " ) > -1;
 }
@@ -55,22 +59,28 @@ dataset = dataset.map( function (d) {
 
 dataset = d3.nest().key(function(d) { return d.name; }).entries(dataset	);
 
-var h=300;
-var w=500;
+var w=$("#chart").width();
+var h=400;
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
+var margin = {top: 20, right: 30, bottom: 20, left: 30},
     width = w - margin.left - margin.right,
     height = h - margin.top - margin.bottom;
 
 var xScale = d3.scale.linear()
-                     .range([margin.left, w-margin.right]);
-xScale.domain([d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.x; }); }), 
-	           d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.x; }); })]).nice();
+                     .range([margin.left, w-margin.right])
+                     .domain([
+                     	d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.x; }); }), 
+	           			d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.x; }); })
+	           		 ])
+	           		 .nice();
 
-var yScale = d3.scale.linear()
-                     .range([margin.bottom, h-margin.top]);
-yScale.domain([d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.y; }); }), 
-	           d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.y; }); })]).nice();
+var yLeft = d3.scale.linear()
+                     .range([margin.bottom, h-margin.top])
+                     .domain([
+                     	d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.y; }); }), 
+	           			d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.y; }); })
+	           		  ])
+                     .nice();
 
 var color = d3.scale.category10()
     .domain(d3.keys(dataset[0]).filter(function(key) { return key === "name"; }));
@@ -78,40 +88,326 @@ var color = d3.scale.category10()
 var xAxis = d3.svg.axis()
     .scale(xScale)
     .orient("bottom")
-    .ticks(5);
+    .ticks(n_xticks)
+    .tickSubdivide(1);
 
 var yAxis = d3.svg.axis()
-    .scale(yScale)
+    .scale(yLeft)
     .orient("left")
     .ticks(5);
 
 var line = d3.svg.line()
-    .interpolate("basis")
+    .interpolate("")
     .x(function(d) { return xScale(d.x); })
-    .y(function(d) { console.log(d); return yScale(d.y); });
+    .y(function(d) { return yLeft(d.y); });
 
 var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", w + margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 svg.append("g")
     .attr("class", "axis")  //Assign "axis" class
-    .attr("transform", "translate(0," + (h - margin.bottom-10) + ")")
+    .attr("id", "xaxis") 
+    .attr("transform", "translate(0," + (h - margin.bottom) + ")")
     .call(xAxis);
 
 svg.append("g")
     .attr("class", "axis")  //Assign "axis" class
+    .attr("id", "yaxis")
     .attr("transform", "translate(" + margin.left + ",0)")
     .call(yAxis);
 
 var scans = svg.selectAll(".scan")
       .data(dataset, function(d) { return d.key; })
       .enter().append("g")
-      .attr("class", "scan");
-
-  scans.append("path")
+      .attr("class", "scan")
+      .append("path")
       .attr("class", "line")
       .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return color(d.key); });
+      .style("stroke", function(d) { return color(d.key); })
+      .style("fill","None");
+
+svg.append("text")
+    .attr("class", "xlabel")
+    .attr("text-anchor", "end")
+    .attr("x", 2*margin.left)
+    .attr("y", height+55)
+    .text("X Label");
+
+svg.selectAll("line.verticalGrid").data(xScale.ticks(n_xticks)).enter()
+    .append("line")
+        .attr(
+        {
+            "class":"verticalGrid",
+            "x1" : function(d){ return xScale(d);},
+            "x2" : function(d){ return xScale(d);},
+            "y1" : height,
+            "y2" : 0,
+            "fill" : "none",
+            "shape-rendering" : "crispEdges",
+            "stroke" : "gray",
+            "stroke-width" : "1px",
+            "opacity":0.5
+        });
+
+var xlabelGroup = svg.append("svg:g")
+		.attr("class", "x-label-group")
+		.append("svg:text")
+		.attr("class", "x-label")
+		.attr("id", "x-label-id")
+		.attr("text-anchor", "end")
+		.attr("opacity", "0.75") 
+		.attr("font-size", "11")
+		.attr("y", -4)
+		.attr("x", w-margin.right)
+		.text('NA')
+				
+var createScaleButtons = function() {
+		var cumulativeWidth = 0;		
+		// append a group to contain all lines
+		var buttonGroup = svg.append("svg:g")
+				.attr("class", "scale-button-group")
+			.selectAll("g")
+				.data(scales)
+			.enter().append("g")
+				.attr("class", "scale-buttons")
+			.append("svg:text")
+				.attr("class", "scale-button")
+				.text(function(d, i) {
+					return d[1];
+				})
+				.attr("font-size", "12") // this must be before "x" which dynamically determines width
+				.attr("fill", function(d) {
+					if(d[0] == yScale) {
+						return "black";
+					} else {
+						return "blue";
+					}
+				})
+				.classed("selected", function(d) {
+					if(d[0] == yScale) {
+						return true;
+					} else {
+						return false;
+					}
+				})
+				.attr("x", function(d, i) {
+					// return it at the width of previous labels (where the last one ends)
+					var returnX = cumulativeWidth;
+					// increment cumulative to include this one
+					cumulativeWidth += this.getComputedTextLength()+5;
+					return returnX;
+				})
+				.attr("y", -4)
+				.on('click', function(d, i) {
+					handleMouseClickScaleButton(this, d, i);
+				});
+	}
+
+var handleMouseClickScaleButton = function(button, buttonData, index) {
+		if(index == 0) {
+			self.switchToLinearScale();
+		} else if(index == 1) {
+			self.switchToPowerScale();
+		} else if(index == 2) {
+			self.switchToLogScale();
+		}
+		
+		// change text decoration
+		svg.selectAll('.scale-button')
+		.attr("fill", function(d) {
+			if(d[0] == yScale) {
+				return "black";
+			} else {
+				return "blue";
+			}
+		})
+		.classed("selected", function(d) {
+			if(d[0] == yScale) {
+				return true;
+			} else {
+				return false;
+			}
+		})
+		
+	}
+
+	this.switchToPowerScale = function() {
+		yScale = 'pow';
+		redrawAxes(true);
+		redrawLines(true);
+		
+		// fire an event that config was changed
+		$(svg).trigger('LineGraph:configModification')
+	}
+
+	this.switchToLogScale = function() {
+		yScale = 'log';
+		redrawAxes(true);
+		redrawLines(true);
+		
+		// fire an event that config was changed
+		$(svg).trigger('LineGraph:configModification')
+	}
+
+	this.switchToLinearScale = function() {
+		yScale = 'linear';
+		redrawAxes(true);		
+		redrawLines(true);
+		
+		// fire an event that config was changed
+		$(svg).trigger('LineGraph:configModification')
+	}
+
+var redrawAxes = function(withTransition) {
+		initY();
+		initX();
+		
+		if(withTransition) {
+			// slide x-axis to updated location
+			svg.selectAll("g #xaxis.axis").transition()
+			.duration(transitionDuration)
+			.ease("linear")
+			.call(xAxis)				  
+		
+			// slide y-axis to updated location
+			svg.selectAll("g #yaxis.axis").transition()
+			.duration(transitionDuration)
+			.ease("linear")
+			.call(yAxisLeft)
+			
+		} else {
+			// slide x-axis to updated location
+			graph.selectAll("g #xaxis.axis")
+			.call(xAxis)				  
+		
+			// slide y-axis to updated location
+			graph.selectAll("g #yaxis.left")
+			.call(yAxisLeft)
+		}
+	}
+	var transitionDuration = 300;
+	var redrawLines = function(withTransition) {
+		/**
+		* This is a hack to deal with the left/right axis.
+		*
+		* See createGraph for a larger comment explaining this. 
+		*
+		* Yes, it's ugly. If you can suggest a better solution please do.
+		*/
+		lineFunctionSeriesIndex  =-1;
+		
+		// redraw lines
+		if(withTransition) {
+			svg.selectAll("g .scan path")
+			.transition()
+				.duration(transitionDuration)
+				.ease("linear")
+				.attr("d", function(d) { return line(d.values); })
+				.attr("transform", null);
+		} else {
+			svg.selectAll("g .scan path")
+				.attr("d", function(d) { return line(d.values); })
+				.attr("transform", null);
+		}
+	}
+
+
+	var tickFormatForLogScale = ",.01f";
+	/*
+	 * Allow re-initializing the y function at any time.
+	 *  - it will properly determine what scale is being used based on last user choice (via public switchScale methods)
+	 */
+	var initY = function() {
+
+		var numAxisLabels = 6;
+		var numAxisLabelsPowerScale = 6;
+		var numAxisLabelsLinearScale = 10;
+		if(yScale == 'pow') {
+			yLeft = d3.scale.pow()
+					.exponent(0.3)
+					.domain([
+						d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.y; }); }), 
+	           			d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.y; }); })
+	           			])
+					.range([margin.bottom, h-margin.top])
+					.nice();	
+			numAxisLabels = numAxisLabelsPowerScale;
+		} else if(yScale == 'log') {
+			// we can't have 0 so will represent 0 with a very small number
+			// 0.1 works to represent 0, 0.01 breaks the tickFormatter
+			yLeft = d3.scale.log()
+				.domain([
+						d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.y; }); }), 
+	           			d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.y; }); })
+	           			])
+				.range([margin.bottom, h-margin.top])
+				.nice();	
+		} else if(yScale == 'linear') {
+			yLeft = d3.scale.linear()
+			.domain([
+						d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.y; }); }), 
+	           			d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.y; }); })
+	           			])
+			.range([margin.bottom, h-margin.top])
+			.nice();
+			numAxisLabels = numAxisLabelsLinearScale;
+		}
+
+		yAxisLeft = d3.svg.axis().scale(yLeft)
+					.ticks(numAxisLabels, tickFormatForLogScale)
+					.orient("left");
+
+	}
+
+	
+	/*
+	 * Allow re-initializing the x function at any time.
+	 */
+	var initX = function() {
+		// X scale starts at epoch time 1335035400000, ends at 1335294600000 with 300s increments
+		xScale = d3.scale.linear()
+                     .range([margin.left+15, w-margin.right])
+                     .domain([
+                     	d3.min(dataset, function(d) { return d3.min(d.values, function (d) { return d.x; }); }), 
+	           			d3.max(dataset, function(d) { return d3.max(d.values, function (d) { return d.x; }); })
+	           			])
+                     .nice();
+		
+		// create yAxis (with ticks)
+		xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(n_xticks).tickSubdivide(1);
+	}
+	getScale = function() {
+		return yScale;
+	}
+
+createScaleButtons();
+var hoverlineXoffset = margin.left+15;
+// Hover line. 
+var hoverLineGroup = svg.append("g")
+					.attr("class", "hover-line");
+var hoverLine = hoverLineGroup
+	.append("line")
+		.attr("x1", 10).attr("x2", 10) 
+		.attr("y1", 0).attr("y2", h); 
+// Hide hover line by default.
+hoverLine.style("opacity", 1e-6);
+var xlabelFormat = d3.format(",.01f");
+// Add mouseover events.
+d3.select("#chart")
+	.on("mouseover", function() { 
+  		console.log('mouseover');
+	})
+	.on("mousemove", function() {
+  		console.log('mousemove', d3.mouse(this));
+  		var x = d3.mouse(this)[0]-hoverlineXoffset;
+  		hoverLine.attr("x1", x).attr("x2", x).style("opacity", 1);
+  		xlabelGroup.text(xlabelFormat(xScale.invert(x)));
+  	})  
+  	.on("mouseout", function() {
+    	console.log('mouseout');
+    	hoverLine.style("opacity", 1e-6);
+  	});
+
