e421abfde123c3899bcfaf4180b169264b1d9559
David Vine <djvine@gmail.com>
2014-02-07 17:08:29 -0600

    Minor cleanup and styling.

--- scans/urls.py
+++ scans/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 
 import scans.views
+import lineplots.views
 
 #Regex named pattern (?P<name>pattern)
 
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     # ex: /scans/
     url(r'^$', scans.views.ScansListView.as_view(), name='scans-list'),
     # ex: /scans/2-ID-B/plots/
-    url(r'^(?P<beamline>.*?)/plots/$', scans.views.plots, name='plots'),
+    url(r'^(?P<beamline>.*?)/plots/$', lineplots.views.lineplots, name='plots'),
     # ex: /scans/2-ID-B/images/
     url(r'^(?P<beamline>.*?)/images/$', scans.views.images, name='images'),
 )
--- webics/urls.py
+++ webics/urls.py
@@ -7,9 +7,5 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'webics.views.home', name='home'),
     url(r'^scans/', include('scans.urls', namespace='scans')),
-    url(r'^test_chat/', 'chat.views.chat'),
-    url(r'^test_det_buttons/', 'det_buttons.views.det_buttons'),
-    url(r'^test_history/', 'history.views.history'),
-    url(r'^test_lineplots/', 'lineplots.views.lineplots'),
     url(r'^admin/', include(admin.site.urls)),
 )
