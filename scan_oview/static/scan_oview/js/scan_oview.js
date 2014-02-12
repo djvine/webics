94573116126dc55f72267560118d04376b9c773b
David Vine <djvine@gmail.com>
2014-02-12 17:36:57 -0600

    Scan overview completed.

--- scan_oview/static/scan_oview/js/scan_oview.js
+++ scan_oview/static/scan_oview/js/scan_oview.js
@@ -8,7 +8,9 @@ $(document).ready(function(){
 		start: new Date(Math.abs(new Date()-345*24*3600*1000)),
 		cellSize: 10,
 		range: 12,
+		tooltip: true,
 		legend: [5, 10, 15, 20],
+		highlight: "now",
 		nextSelector: "#domainDynamicDimension-next",
 		previousSelector: "#domainDynamicDimension-previous"
 	});
--- scan_oview/views.py
+++ scan_oview/views.py
@@ -12,31 +12,46 @@ from datetime import datetime, timedelta
 # Create your views here.
 def scan_oview(request, beamline='DJV'):
 
-    td_year = timedelta(365)
-    td_month = timedelta(30)
-    td_week = timedelta(7)
-    td_day = timedelta(1)
+    days = [1,2,30,365]
+    n_scans = []
+    n_points = []
+
+    all_items = Scan.objects.filter(beamline=beamline)
+    for day in days:
+        items = all_items.filter(ts__gt=pytz.timezone('America/Chicago').localize(datetime.now())-timedelta(day))
+        n_scans.append(len(items))
+        tmp_pts = 0
+        for item in items:
+            try:
+                if len(item.history.values())==1:
+                    tmp_pts+=item.history.values()[0]['completed']
+                else:
+                    tmp_pts+=item.history.values()[0]['completed']
+                    if item.history.values()[1]['completed']>1:
+                        tmp_pts+=(item.history.values()[1]['completed']-1)*item.history.values()[0]['requested']
+            except IndexError:
+                pass
+        n_points.append(tmp_pts)
+    n_scans.append(len(all_items))
+    tmp_pts = 0
+    for item in all_items:
+        try:
+            if len(item.history.values())==1:
+                tmp_pts+=item.history.values()[0]['completed']
+            else:
+                tmp_pts+=item.history.values()[0]['completed']
+                if item.history.values()[1]['completed']>1:
+                    tmp_pts+=(item.history.values()[1]['completed']-1)*item.history.values()[0]['requested']
+        except IndexError:
+            pass
+    n_points.append(tmp_pts)
 
     scan_chrono = {}
     for item in Scan.objects.filter(beamline=beamline).filter(ts__gt=pytz.timezone('America/Chicago').localize(datetime.now())-timedelta(365)):
         scan_chrono[int(item.ts.strftime('%s'))] = 1
 
-    scans = []
-    points = []
-    area = []
-
-    # day
-    items = Scan.objects.filter(beamline=beamline).filter(ts__gt=pytz.timezone('America/Chicago').localize(datetime.now())-td_day):
-    scans.append(len(items))
-    for item in items:
-        if len(item.history.values)==1: #1D scan
-            points.append(items.history.values()[0]['completed'])
-        else: # 2D scan
-            points.append()
-
-
     recent_scans = Scan.objects.filter(beamline=beamline).order_by('-ts')[:20]
     beamlines = sorted([{'beamline': bl} for bl in scans.config.ioc_names.keys()])
     context = {'title': 'Webics: {:s} Overview'.format(beamline), 'beamlines': beamlines, 'active_tab': beamline, 
-                'data': json.dumps(scan_chrono), 'recent_scans': recent_scans}
+                'data': json.dumps(scan_chrono), 'recent_scans': recent_scans, 'scans': n_scans, 'points': n_points}
     return render(request, 'scan_oview/scan_oview.html', context)
