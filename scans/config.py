f2128c872719e70db9a9f30bc75277f46e5a4ee0
David J. Vine <djvine@gmail.com>
2014-05-09 13:30:26 -0500

    Prepare for deployment to apache server

--- scans/config.py
+++ scans/config.py
@@ -2,7 +2,7 @@
 import socket
 hostname = socket.gethostname()
 
-if hostname in ['lemon.xray.aps.anl.gov', 'joule.xray.aps.anl.gov']:
+if hostname in ['lemon.xray.aps.anl.gov', 'joule-vm.xray.aps.anl.gov']:
 	ioc_names = {
 		'2-ID-B': '2idb1',
 		'2-ID-E': '2xfm',
