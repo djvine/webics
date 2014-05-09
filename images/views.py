0f741f2ac073a94b08d9d9f5648cd5f01ed02f5f
David J. Vine <djvine@gmail.com>
2014-05-09 13:39:22 -0500

    Prepare for deployment to apache server

--- images/views.py
+++ images/views.py
@@ -15,9 +15,9 @@ def images(request, beamline='DJV'):
     try:
         s = recent_scans[0]
         cache['scan'] = {'scan_id': s.scan_id, 'ts': s.ts.strftime("%a %d %b %H:%M")}
-        cache['scan_hist'] = [ {'dim': entry['dim'], 'completed': entry['completed'], 'requested':entry['requested']} for 
+        cache['scan_hist'] = [ {'dim': entry['dim'], 'completed': entry['completed'], 'requested':entry['requested']} for
                             entry in s.history.values()]
-        cache['scan_dets'] = ['D{:02d}'.format(entry['active']) for entry in s.detectors.values()]
+        cache['scan_dets'] = ['D{0:02d}'.format(entry['active']) for entry in s.detectors.values()]
         cache['scan_metadata'] = [{'pvname':entry['pvname'], 'value':entry['value']} for entry in s.metadata.values()]
         cache['scan_data'] = {}
         for entry in s.data.values():
@@ -30,13 +30,13 @@ def images(request, beamline='DJV'):
                     cache['scan_data'][entry['row']]
                 except:
                     cache['scan_data'][entry['row']] = []
-                cache['scan_data'][entry['row']].append({'name': entry['pvname'].split('.')[1][:3], 
+                cache['scan_data'][entry['row']].append({'name': entry['pvname'].split('.')[1][:3],
                                                          'values': cPickle.loads(str(entry['value']))})
     except:
         print 'Error: Unable to retrieve data'
 
     beamlines = sorted([{'beamline': bl} for bl in scans.config.ioc_names.keys()])
-    dets = dets = ['D{:02d}'.format(i) for i in range(1, 71)]
-    context = {'title': 'Webics: {:s} Images'.format(beamline), 'beamlines': beamlines, 'active_tab': beamline, 
+    dets = dets = ['D{0:02d}'.format(i) for i in range(1, 71)]
+    context = {'title': 'Webics: {0:s} Images'.format(beamline), 'beamlines': beamlines, 'active_tab': beamline,
                 'data': json.dumps(cache), 'dets': dets, 'recent_scans': recent_scans}
     return render(request, 'images/images.html', context)
--- lineplots/views.py
+++ lineplots/views.py
@@ -19,7 +19,7 @@ def lineplots(request, beamline='DJV'):
         cache['scan'] = {'scan_id': s.scan_id, 'ts': s.ts.strftime("%a %d %b %H:%M")}
         cache['scan_hist'] = [ {'dim': entry['dim'], 'completed': entry['completed'], 'requested':entry['requested']} for
                             entry in s.history.values()]
-        cache['scan_dets'] = ['D{:02d}'.format(entry['active']) for entry in s.detectors.values()]
+        cache['scan_dets'] = ['D{0:02d}'.format(entry['active']) for entry in s.detectors.values()]
         cache['scan_metadata'] = [{'pvname':entry['pvname'], 'value':entry['value']} for entry in s.metadata.values()]
         cache['scan_data'] = {}
         for entry in s.data.values():
@@ -41,7 +41,7 @@ def lineplots(request, beamline='DJV'):
         raise
 
     beamlines = sorted([{'beamline': bl} for bl in scans.config.ioc_names.keys()])
-    dets = dets = ['D{:02d}'.format(i) for i in range(1, 71)]
-    context = {'title': 'Webics: {:s} Line Plots'.format(beamline), 'beamlines': beamlines, 'active_tab': beamline,
+    dets = dets = ['D{0:02d}'.format(i) for i in range(1, 71)]
+    context = {'title': 'Webics: {0:s} Line Plots'.format(beamline), 'beamlines': beamlines, 'active_tab': beamline,
             'data': json.dumps(cache), 'dets': dets, 'recent_scans': recent_scans, 'env': settings.SOCKET_ENV}
     return render(request, 'lineplots/lineplots.html', context)
