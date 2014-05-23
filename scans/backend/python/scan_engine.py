9d5835b5b73f7bb8c71258a62685df5eee304ee0
David J. Vine <djvine@gmail.com>
2014-05-23 17:20:59 -0500

    Prepare for deployment to apache server

--- scans/backend/python/scan_engine.py
+++ scans/backend/python/scan_engine.py
@@ -471,7 +471,7 @@ class ScanListener(threading.Thread):
                     for i in range(n_pix):
                         tsum = np.zeros(pix_per_buff)
                         for elem in range(4): #Detector elements
-                            tsum[i] += sp.sum(buff[512+i*8448+elem*2048+xfd_dets[detector][elem*2]:512+i*8448+elem*2048+xfd_dets[detector][elem*2+1]])
+                            tsum[i] += np.sum(buff[512+i*8448+elem*2048+xfd_dets[detector][elem*2]:512+i*8448+elem*2048+xfd_dets[detector][elem*2+1]])
                         if i_buffs == 0:
                             cache['scan_data']['{:d}'.format(row)].append({
                                 'name': detector,
