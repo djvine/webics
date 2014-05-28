6b7a11e6ef07c2b296b27a24053276203058e414
David J. Vine <djvine@gmail.com>
2014-05-28 09:35:47 -0500

    Prepare for deployment to apache server

--- scans/backend/python/scan_engine.py
+++ scans/backend/python/scan_engine.py
@@ -174,7 +174,7 @@ class ScanListener(threading.Thread):
                 self.xfd_pref+':PixelsPerBuffer_RBV',
                 self.xfd_pref+':image1:ArrayData',
                 self.xfd_pref+':image1:ArraySize0_RBV',
-                self.xfd_pref+':netCDF1:NumCaptured_RBV',
+                self.xfd_pref+':image1:UniqueId_RBV',
                 ])
             for detector in scans.config.fly_det_config[self.beamline].keys():
                 if scans.config.fly_det_config[self.beamline][detector]!='normal':
@@ -468,7 +468,7 @@ class ScanListener(threading.Thread):
         pix_per_buff = self.pvs[self.xfd_pref+':PixelsPerBuffer_RBV'].get()
         n_buffs = np.int(np.ceil(x_dim%pix_per_buff)) # Number of buffs per row
         i_buffs = 0
-        buffs_uid = self.pvs[self.xfd_pref+':netCDF1:NumCaptured_RBV'].get() # Used to determine if there is a new buffer available
+        buffs_uid = self.pvs[self.xfd_pref+':image1:UniqueId_RBV'].get()+1 # Used to determine if there is a new buffer available
         buff_size = self.pvs[self.xfd_pref+':image1:ArraySize0_RBV'].get()
         cache_pos = {}
         row = 0
@@ -479,7 +479,7 @@ class ScanListener(threading.Thread):
             # During row accumulate buffers and convert to pixels & spectra
             # At end of line get multi-channel scaler info
             c_buffs_uid = self.pvs[self.xfd_pref+':netCDF1:NumCaptured_RBV'].get()
-            if c_buffs_uid!=buffs_uid: # New buffer available
+            if c_buffs_uid>buffs_uid: # New buffer available
                 buffs_uid = c_buffs_uid
                 if i_buffs==0:
                     cache['scan_data']['{:d}'.format(row)]=[]
